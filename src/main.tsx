06c06ed644ec14c95a56da4d0099e8b9b9ecb80d
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-03-18 15:20:50 +0300

    setup for development outside telegram

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { init, backButton } from "@telegram-apps/sdk-react";
+import { AppRouter } from "./routers";
+import { checkIfInsideTelegram, mockTelegramEnvSetup } from "./helpers";
+
+mockTelegramEnvSetup();
+
+checkIfInsideTelegram(); // Выполнить проверку при старте приложения
 
 // Инициализируем SDK
 init();
@@ -8,4 +13,4 @@ init();
 // Монтируем кнопку "Назад" (если она нужна)
 backButton.mount();
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<AppRouter />);
